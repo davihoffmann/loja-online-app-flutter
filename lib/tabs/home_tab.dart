8d074d71488eedae3012c0f16f6979bbf4f2a3fe
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-23 09:55:24 -0300

    criado o futereBuilder que vai receber as imagens do firebase

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,22 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     Widget _buildBodyBack() => Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 211, 118, 130),
-            Color.fromARGB(255, 253, 181, 168)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )
-      ),
-    );
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 211, 118, 130),
+              Color.fromARGB(255, 253, 181, 168)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+        );
 
     return Stack(
       children: <Widget>[
@@ -32,12 +30,34 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
-            )
+            ),
+            FutureBuilder<QuerySnapshot>(
+                future: Firestore.instance
+                    .collection("home")
+                    .orderBy("pos")
+                    .getDocuments(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      ),
+                    );
+                  } else {
+                    print(snapshot.data.documents.length);
+                    return SliverToBoxAdapter(
+                      child: Container(),
+                    );
+                  }
+                })
           ],
         )
       ],
     );
-
   }
-
 }
