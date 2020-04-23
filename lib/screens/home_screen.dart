1e43bc23d8fedeaaf03ad9ca2c4f7d4e0b8cfe93
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-23 09:11:18 -0300

    verificado a utilizacao do pageview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+
+    //_pageController.animateToPage(page, duration: null, curve: null);
+    //_pageController.jumpToPage(page)
+
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(color: Colors.red,),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,)
+      ],
     );
+
   }
+
 }
