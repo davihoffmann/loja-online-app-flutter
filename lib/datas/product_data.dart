1e7de918766a6af2839ef54cce3e726454b3d640
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-23 19:19:36 -0300

    criada classe de produtos

---
+++ lib/datas/product_data.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData {
+  
+  String id;
+  String title;
+  String description;
+  double price;
+  List images;
+  List sizes;
+  String category;
+
+  ProductData.fromDocument(DocumentSnapshot snapshot) {
+    this.id = snapshot.documentID;
+    this.title = snapshot.data['title'];
+    this.description = snapshot.data['description'];
+    this.price = snapshot.data['price'] + 0.0;
+    this.sizes = snapshot.data['sizes'];
+    this.images = snapshot.data['images'];
+  }
+
+}
