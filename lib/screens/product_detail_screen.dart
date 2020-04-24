a48246ec134a70825f3e35548cb9551b16b3336e
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-23 23:21:24 -0300

    adicionado plugin carousel para exibitar as imagens do produto

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,8 +1,8 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/datas/product_data.dart';
 
 class ProductDetailScreen extends StatefulWidget {
-
   final ProductData product;
 
   ProductDetailScreen(this.product);
@@ -12,17 +12,59 @@ class ProductDetailScreen extends StatefulWidget {
 }
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
-
   final ProductData product;
 
   _ProductDetailScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
+    final Color primatyColor = Theme.of(context).primaryColor;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return Image.network(url);
+              }).toList(),
+              dotSize: 5.0,
+              dotSpacing: 15.0,
+              //dotBgColor: Colors.transparent,
+              dotColor: primatyColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    color: primatyColor
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
