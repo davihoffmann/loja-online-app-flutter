2677498da53e132e9580cbbd19ef6710d9e31358
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-28 22:37:39 -0300

    criada tela de login do app

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrar"),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              "Criar Conta",
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+            textColor: Colors.white,
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formkey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              validator: (text) {
+                if (text.isEmpty || !text.contains('@')) {
+                  return "E-mail inválido";
+                }
+              },
+              decoration: InputDecoration(hintText: "E-mail"),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              validator: (text) {
+                if (text.isEmpty || text.length < 6) {
+                  return "Senha inválida";
+                }
+              },
+              decoration: InputDecoration(hintText: "Senha"),
+              obscureText: true,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Esqueci minha senha",
+                  textAlign: TextAlign.right,
+                ),
+                padding: EdgeInsets.zero,
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            SizedBox(
+              height: 45.0,
+              child: RaisedButton(
+                onPressed: () {
+                  if(_formkey.currentState.validate()) {
+                    
+                  }
+                },
+                child: Text(
+                  "Entrar",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
